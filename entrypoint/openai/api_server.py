5ecf3aeb27355f89febfa297bf3d216e17b188b8
clark <panf2333@gmail.com>
2025-01-26 18:27:58 +0800

    fix

--- entrypoint/openai/api_server.py
+++ entrypoint/openai/api_server.py
@@ -84,11 +84,10 @@ async def health(raw_request: Request) -> Response:
     """Health check."""
     return Response(status_code=200)
 
-
 @router.api_route("/v1/images/generations", methods=["GET", "POST"])
 async def imagesGenerations(req: CreateImageRequest, raw_req: Request) -> Response:
     """Ping check. Endpoint required for SageMaker"""
-    image = raw_req.app.state.pipeline(req.prompt, req.num_inference_steps, req.guidance_scale).images[0]
+    image = raw_req.app.state.pipeline(req.prompt, num_inference_steps=req.num_inference_steps, guidance_scale=req.guidance_scale).images[0]
     path = f"output-{uuid.uuid4()}.png"
     logger.info(f"Saving image to {path}")
     image.save(path)
